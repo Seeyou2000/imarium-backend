d937722bea7005c12d343f71f9f04edf68f76171
Kimsiwoo <valentine0517@naver.com>
2024-09-13 14:02:27 +0900

    project init

---
+++ src/main/java/com/imarium/ImariumBackendApplication.java
@@ -0,0 +1,13 @@
+package com.imarium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImariumBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImariumBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/imarium/ImariumBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imarium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImariumBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
